deebab6fd24c2ec674c4d3808d5d19cbbaa8cb9d
dppizza@gmail.com <dppizza@gmail.com@424afc62-656d-f10e-c7b8-71cae8145af3>
2013-02-22 19:43:50 +0000

    interface for gnuplot in java, maven project

---
+++ trunk/develop/javagnuplot/src/main/java/es/ugr/decsai/jpons/javagnuplot/App.java
@@ -0,0 +1,13 @@
+package es.ugr.decsai.jpons.javagnuplot;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
